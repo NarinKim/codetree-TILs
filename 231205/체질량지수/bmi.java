65d4511564bb46716e50b057414f90c6ab8b58ac
Narin Kim <97506603+NarinKim@users.noreply.github.com>
2023-12-05 15:27:38 +0900

    Title: 체질량지수, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231205/체질량지수/bmi.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 키
+        int n2 = sc.nextInt(); //몸무게
+
+        double bmi = n2/((n*0.01)*(n*0.01));
+
+        if(bmi >= 25) {
+            System.out.println((int)bmi);
+            System.out.print("Obesity");
+        } else {
+            System.out.print(bmi);
+        }
+    }
+}
