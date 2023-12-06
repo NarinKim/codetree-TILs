3eaa7a60794c88918997bb9a5937fd460b465f86
Narin Kim <97506603+NarinKim@users.noreply.github.com>
2023-12-06 17:04:51 +0900

    Title: 시력 검사 2, Time: 175ms, Memory: 11MB, Status: Passed - CodeTree

--- 231206/시력 검사 2/eye-test-2.java
+++ 231206/시력 검사 2/eye-test-2.java
@@ -9,9 +9,9 @@ public class Main {
         if(a >= 1.0) {
             System.out.println("High");
         } else if(a >= 0.5) {
-            System.out.println("middle");
+            System.out.println("Middle");
         } else {
-            System.out.println("low");
+            System.out.println("Low");
         }
     }
 }
