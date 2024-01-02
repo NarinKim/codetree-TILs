8b363f5b4996115c1809e84d6bc58362d1072e1f
Narin Kim <97506603+NarinKim@users.noreply.github.com>
2024-01-02 17:05:15 +0900

    Title: 숫자의 계절은, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240102/숫자의 계절은/season-of-num.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        
+        if(m>=3 && m<=5){
+            System.out.print("Spring");
+        } else if(m<=8 && m>=6) {
+            System.out.print("Summer");
+        } else if(m>=9 && m<=11) {
+            System.out.print("Fall");
+        } else {
+            System.out.print("Winter");
+        }
+
+
+    }
+}
